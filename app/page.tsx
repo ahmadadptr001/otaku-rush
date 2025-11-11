e1d5c1fa2e2891d14688b163d7308f39c13530d8
Ahmad Adptr <ahmadadptr@gmail.com>
2025-11-11 09:42:25 +0800

    ui: update ui bug

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,11 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col items-center">
-        <Nav />
         <div className="flex-1 w-full flex flex-col gap-20">
           <Hero />
         </div>
         <PremiumPicker />
         <Question />
-        <Footer />
       </div>
     </main>
   );
